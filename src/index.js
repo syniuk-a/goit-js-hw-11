17f08757ce260725c9f21a0f7be5e99edf12db2f
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-09-09 19:57:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 import ImagesAPIService from './js/APIService';
 import Markup from './js/markup';
-import ImagesAPIService from './js/APIService';
 
 const formSearch = document.querySelector('.search-form');
 const galleryRef = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
 
-const ImagesAPIService = new ImagesAPIService();
+const imagesAPIService = new ImagesAPIService();
 const renderMarkup = new Markup({ selector: galleryRef });
 
 formSearch.addEventListener('submit', onFormSearch);
