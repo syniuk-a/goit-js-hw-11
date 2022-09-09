be3ec6b25174a72be1f733af5364f750a53bd64f
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-09-09 13:44:07 +0300

    Update APIService.js

--- src/js/APIService.js
+++ src/js/APIService.js
@@ -5,4 +5,22 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '22757150-c2d7916cb8ffee93e4314d78c';
 
+export default class ImagesAPIService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+    this.totalHits = null;
+    this.totalPages = null;
+    this.endOfHits = false;
+  }
 
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  
+}
