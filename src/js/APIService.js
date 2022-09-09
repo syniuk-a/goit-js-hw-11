dd4af5e543fb981d07e80cbfdd53f32e06e825ca
Andrii <78295684+syniuk-a@users.noreply.github.com>
2022-09-09 13:39:24 +0300

    Update APIService.js

---
+++ src/js/APIService.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import Notiflix from "notiflix";
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '22757150-c2d7916cb8ffee93e4314d78c';
+
+
